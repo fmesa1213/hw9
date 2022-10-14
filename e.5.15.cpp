2925c2a6c60d5658bd7ac8eef2ab76306c9a44bb
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-10-13 22:25:40 -0400

    Create e.5.15.cpp

---
+++ e.5.15.cpp
@@ -0,0 +1,16 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Homework E5.15
+
+I wrote a function /*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Homework E2.10 - Cost per 100 Miles
+Asks the user for the number of gallons of gas in their tank, the fuel effiency in miles per gallon, and the price of gas per gallon.
+It then prints the cost per 100 miles and how far the car can go with the gas.
+*/
+
+*/
